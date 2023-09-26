fbf7b3a4b67adfe3e785cdcb96277806c1d3460a
Matteo Paone <matte63@users.noreply.github.com>
2023-09-26 18:44:18 +0200

    added cli arguments and minor refactor

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
 #! /usr/bin/python3
 
+import argparse
+
 import mmind
 
 if __name__ == '__main__':
-	mmind.play("qwerasdf", 4, 10)
+
+	parser = argparse.ArgumentParser(
+		prog = 'MMind Game CLI'
+		)
+
+	group = parser.add_mutually_exclusive_group()
+
+	group.add_argument(
+		'-s',
+		'--letters-set',
+		default = "qwerasdf",
+		type = str,
+		help = "input the set of letters you can type (default: %(default)s)"
+		)
+
+	group.add_argument(
+		'-m',
+		'--max-guesses',
+		default = 10,
+		type = int,
+		help = "set guesses limit (default: %(default)s)"
+		)
+
+	group.add_argument(
+		'-l',
+		'--guess-length',
+		default = 4,
+		type = int,
+		help = "set guess length (default: %(default)s)"
+		)
+
+	args = parser.parse_args()
+
+	mmind.play( args.letters_set, args.guess_length, args.max_guesses )
--- mmind.py
+++ mmind.py
@@ -1,7 +1,45 @@
 import os
 import random
 
-import aux
+
+
+def find(string, ch):
+	return [i for i, c in enumerate(string) if c == ch]
+
+def check(string, guess, black='b', white='w'):
+
+	matches = ''
+	res = ''
+
+	for ch in guess:
+
+		fg = find(guess, ch)
+		fs = find(string, ch)
+
+		if fs:
+
+			if ch not in matches:
+
+				if fg == fs:
+					res += black * min(len(fg), len(fs))
+
+				else:
+					res_b = ''
+					res_w = ''
+
+					for i in fg:
+
+						if i in fs:
+							res_b += black
+						else:
+							res_w += white
+
+					res += res_b + white * min(len(fs) - len(res_b), len(res_w))
+
+			matches += ch
+
+
+	return ''.join(sorted(res))
 
 def play(letters = "QWERTY", length = 4, total_guesses = 16):
 
@@ -27,7 +65,7 @@ def play(letters = "QWERTY", length = 4, total_guesses = 16):
 		if len(guess) != length:
 			print(f"Your guess must be {length} letters long.")
 		elif guess.upper() != sequence.upper():
-			print(f"Check: {aux.check(sequence.upper(), guess.upper(), black, white).upper()}")
+			print(f"Check: {check(sequence.upper(), guess.upper(), black, white).upper()}")
 			guesses_left += -1
 		else:
 			print("You did it!")
